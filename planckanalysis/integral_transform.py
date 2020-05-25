5b99498ad1dc19b5e5b2c00ff7802e70f5fa2fbe
Juehang Qin <qinjuehang@hotmail.com>
2020-05-25 18:37:20 -0400

    more skeleton functions in integral_transform.py

--- planckanalysis/integral_transform.py
+++ planckanalysis/integral_transform.py
@@ -1,5 +1,16 @@
-def transform(data):
+'''integral transform module.'''
+from numba import njit
+import numpy as np
+
+def signal_function(vector_in):
+    '''signal template function. Should take into account response function!'''
+
+def generate_alphas():
+    '''generate evenly spaced alpha0 and alpha1 vectors. (Even on 4D cylinder)'''
+
+
+def transform(data,):
     '''Takes time series data as an input and generates a signal value based on
     entry and exit 4-vectors on a sphere extended in time. Returns the signal
-    value and 4-vectors. Refer to Qin's note for a much more detailed 
+    value and 4-vectors. Refer to Qin's note for a much more detailed
     explanation.'''
