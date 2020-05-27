8dbd7ab787426b136d53b0b66e77387f03821182
Juehang Qin <qinjuehang@hotmail.com>
2020-05-27 18:05:37 -0400

    a few more functions in integral_transform.py

--- planckanalysis/integral_transform.py
+++ planckanalysis/integral_transform.py
@@ -2,8 +2,28 @@
 from numba import njit
 import numpy as np
 
-def signal_function(vector_in):
-    '''signal template function. Should take into account response function!'''
+@njit
+def delta_response(delta_t):
+    '''the response function is defined in terms of the number of samples between
+    the source time and the observer time (ie. time of current sample).
+    '''
+    if delta_t == 0:
+        return 1
+    return 0
+
+@njit
+def signal_function(vector_delta, response_func=delta_response):
+    '''signal template function. Should take into account response function!
+    vector_delta.shape == (4,n), for n inputs.
+    '''
+    out = np.zeros((3, vector_delta.shape[1]))
+    for i in range(vector_delta.shape[1]):
+        denom = (vector_delta[1, i]**2 + vector_delta[2, i]**2 + vector_delta[3, i]**2)**(3/2)
+        out[0, i] = (response_func(vector_delta[0, i]) *
+                     vector_delta[1, i]/denom
+                    )
+    return out
+
 
 def generate_alphas():
     '''generate evenly spaced alpha0 and alpha1 vectors. (Even on 4D cylinder)'''
